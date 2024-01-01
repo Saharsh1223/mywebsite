22a957a767065205952d4ab841140661fd3bf588
Saharsh1223 <88390577+Saharsh1223@users.noreply.github.com>
2024-01-01 16:49:08 +0530

    Add new video

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,9 +20,9 @@ export default async function ProjectsPage() {
     return acc;
   }, {} as Record<string, number>);
 
-  const featured = allProjects.find((project) => project.slug === "fps-fullbody")!;
-  const top2 = allProjects.find((project) => project.slug === "modular-fps-multiplayer")!;
-  const top3 = allProjects.find((project) => project.slug === "discord-bot-tutorial-2")!;
+  const featured = allProjects.find((project) => project.slug === "discord-bot-tutorial-3")!;
+  const top2 = allProjects.find((project) => project.slug === "fps-fullbody")!;
+  const top3 = allProjects.find((project) => project.slug === "modular-fps-multiplayer")!;
   const sorted = allProjects
     .filter((p) => p.published)
     .filter(
