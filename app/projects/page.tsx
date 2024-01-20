11bc20d859e8da0cdaa344dda1c890f455d65b95
Saharsh1223 <88390577+Saharsh1223@users.noreply.github.com>
2024-01-20 15:29:23 +0530

    Add FPS Project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,7 +20,7 @@ export default async function ProjectsPage() {
     return acc;
   }, {} as Record<string, number>);
 
-  const featured = allProjects.find((project) => project.slug === "discord-bot-tutorial-3")!;
+  const featured = allProjects.find((project) => project.slug === "fps")!;
   const top2 = allProjects.find((project) => project.slug === "fps-fullbody")!;
   const top3 = allProjects.find((project) => project.slug === "modular-fps-multiplayer")!;
   const sorted = allProjects
